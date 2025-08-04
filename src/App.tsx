694b90ae8916cde80cc3ba5503bb0895435f76e9
Shubham Bajaj <shubham2bajaj@gmail.com>
2025-08-04 21:13:54 +0200

    set up boiler plate

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>BOILER PLATE</h1>
+      <p>Enough to start building web apps</p>
+      <ul>
+        <li>VITE</li>
+        <li>REACT</li>
+        <li>TYPESCRIPT</li>
+        <li>TAILWINDCSS</li>
+        <li>SHADCN(slate)</li>
+        <li>LUCIDE ICONS</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
